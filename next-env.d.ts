f21b3692fb6e9b33a9f87e31af31ddd143d56cec
bhanu prakash <bhanuprakashr129@gmail.com>
2025-01-29 06:29:12 +0530

    feat: add biome

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,8 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/pages/api-reference/config/typescript for more information.
+module.exports = {
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  };
